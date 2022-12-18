95ec65734faf4d5abed6fc6e68711043c011e246
trigpolynom <trigpolynom@gmail.com>
2022-12-18 16:14:07 -0500

    starting soap client

--- core/src/main/java/org/odata/neo/java/core/Services/Foundations/Tokenizations/TokenizationServiceImpl.java
+++ core/src/main/java/org/odata/neo/java/core/Services/Foundations/Tokenizations/TokenizationServiceImpl.java
@@ -1,7 +1,9 @@
 package org.odata.neo.java.core.Services.Foundations.Tokenizations;
 
 import java.util.List;
+import java.util.function.Function;
 
+import org.apache.commons.lang3.StringUtils;
 import org.odata.neo.java.core.Models.Tokens.Token;
 
 public class TokenizationServiceImpl implements TokenizationService {
@@ -16,16 +18,26 @@ public class TokenizationServiceImpl implements TokenizationService {
         }
     }
 
-    private static Iterable<Token> oTokenize(String rawQuery, List<Character> sepChars) {
+    private static Iterable<Token> oTokenize(String rawQuery, Character[] sepChars) {
         String remainingRawQuery = rawQuery;
         Boolean notSepChar = false;
 
-        for (Character c : sepChars) {
-            if (!sepChars.contains(c)) {
-                notSepChar = true;
-            }
+        
 
+        while (remainingRawQuery.length() > 0) {
+            String returnValue = remainingRawQuery;
+            String nextRemainingValue = "";
+
+            Integer index = StringUtils.indexOfAny(sepChars, null)
         }
+    }
+
+    private static Integer getRangeIndex(Integer index) {
+        if (index > 0) {
+            return index;
+        }
+
+        return 1;
     }
     
 }
