a36af52b76972611e4accd0078d85362fc0996a4
trigpolynom <trigpolynom@gmail.com>
2023-01-18 21:34:18 -0500

    added projectedType

---
+++ parser/src/main/java/org/odata/neo/parser/Models/ProjectedTokens/ProjectedTokenType.java
@@ -0,0 +1,13 @@
+package org.odata.neo.parser.Models.ProjectedTokens;
+
+public enum ProjectedTokenType {
+
+    Unidentified,
+    Keyword,
+    Assignment,
+    Property,
+    Space,
+    Equals,
+    Comma
+
+}
