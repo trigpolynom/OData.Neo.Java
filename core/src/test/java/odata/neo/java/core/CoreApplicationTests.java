941ec26ccdf045aea330acd693fdab2c065bf0e1
trigpolynom <trigpolynom@gmail.com>
2023-03-08 22:38:53 -0500

    fixed directory

---
+++ core/src/test/java/odata/neo/java/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package odata.neo.java.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
