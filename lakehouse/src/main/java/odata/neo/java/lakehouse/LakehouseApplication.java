84e61c918cd855d427f139e38f95c5f67064ec24
trigpolynom <trigpolynom@gmail.com>
2023-03-11 21:50:42 -0500

    added lakehouse to start

---
+++ lakehouse/src/main/java/odata/neo/java/lakehouse/LakehouseApplication.java
@@ -0,0 +1,13 @@
+package odata.neo.java.lakehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LakehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LakehouseApplication.class, args);
+	}
+
+}
---
+++ lakehouse/src/test/java/odata/neo/java/lakehouse/LakehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package odata.neo.java.lakehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LakehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
